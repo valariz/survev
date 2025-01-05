0616d4895210e6a432f26af854bef13730d2e669
Drino955 <dima.nikiforov.2017.2@gmail.com>
2025-01-05 16:27:55 +0100

    fix: spinbot key changed from x to c

--- krityhack.user.js
+++ krityhack.user.js
@@ -136,7 +136,7 @@ window.addEventListener('keyup', function (event) {
             event.stopPropagation();
             event.preventDefault();
             break;
-        case 'X': spinBot = !spinBot; break;
+        case 'C': spinBot = !spinBot; break;
         // case 'O': window.gameOptimization = !window.gameOptimization; break;
     }
     updateOverlay();
@@ -715,7 +715,7 @@ function updateOverlay() {
         [ '[B] AimBot:', aimBotEnabled ],
         [ '[Z] Zoom:', zoomEnabled ],
         [ '[M] MobileAtk:', mobileAttackEnabled ],
-        [ '[X] SpinBot:', spinBot ],
+        [ '[C] SpinBot:', spinBot ],
         // [ '[O] gameOptimization:', gameOptimization ],
     ];
 
@@ -752,7 +752,110 @@ const ammo = [
     },
 ]
 
-
+function autoSwitch(){
+    var curWeapIdx = dataAccessor.GetPlayerWeapIdx(player);
+    var weaps = dataAccessor.GetPlayerWeapons(player);
+    var curWeap = weaps[curWeapIdx];
+    var sniper = () => {
+        var shouldSwitch = gun => {
+            var s = false
+            try {
+                s =
+                    (this.option("sniper")
+                        ? data.guns[gun].fireMode === "single" ||
+                        data.guns[gun].fireMode === "burst"
+                        : true) &&
+                    data.guns[gun].fireDelay >=
+                        (typeof this.option("speed") == "number"
+                            ? this.option("speed") / 100
+                            : 0.45);
+            }
+            catch (e) {
+            console.error("Sniper Switch",e);
+            }
+            return s;
+        }
+        this.keys = [
+            weaps["1"].type !== "" && shouldSwitch(weaps["1"].type)
+                ? "EquipOtherGun"
+                : this.option("melee")
+                ? "EquipMelee"
+                : "EquipOtherGun",
+            weaps["0"].type !== "" && shouldSwitch(weaps["0"].type)
+                ? "EquipOtherGun"
+                : this.option("melee")
+                ? "EquipMelee"
+                : "EquipOtherGun",
+            "EquipMelee",
+            "EquipMelee",
+        ];
+        if(curWeap.ammo !== this.ammo[curWeapIdx].ammo) {
+            if (curWeap.ammo < this.ammo[curWeapIdx].ammo &&
+                shouldSwitch(curWeap.type) &&
+                curWeap.type == this.ammo[curWeapIdx].type) 
+            {
+                input.addInput(this.keys[curWeapIdx])
+            }
+            this.ammo[curWeapIdx].ammo = curWeap.ammo
+            this.ammo[curWeapIdx].type = curWeap.type
+        }
+    }
+    if(this.option("mode") == "SniperSwitch™") {
+        sniper()
+    }
+    else if(this.option("mode") == "SmartSwitch™") {
+        let choices = [];
+        if(data.autoAttack) {
+            return;
+        }
+        for(let i = 0; i < 2; i++) {
+            let enemy = data.selectedEnemy[0],
+                gun = data.guns[weaps[i].type],
+                bullet;
+            if(gun) {
+                bullet = data.bullets[gun.bulletType];
+            }
+            if(!enemy || !bullet) {
+                break;
+            }
+            var enemyPos = dataAccessor.GetPlayerPosition(enemy);
+            var playerPos = dataAccessor.GetPlayerPosition(player);
+            let distance = this.calcDistance(
+                enemyPos.x,
+                enemyPos.y,
+                playerPos.x,
+                playerPos.y
+            )
+            choices.push({
+                dps:
+                    ((1 / gun.fireDelay) *
+                        bullet.damage *
+                        (gun.burstCount || 1) *
+                        gun.bulletCount *
+                        (1 - bullet.falloff)) ^
+                    distance,
+                hittable: distance < bullet.distance,
+                hasAmmo: weaps[i].ammo > 0,
+                key: i,
+                gun,
+            })
+        }
+        if(choices.length == 2) {
+            choices = choices.filter(function (e) {
+                return e.hittable && e.hasAmmo
+            });
+            choices = choices.sort(function (a, b) {
+                return b.dps - a.dps
+            });
+            if(!choices[0]) {
+                return;
+            }
+            input.addInput(
+                choices[0].key == 0 ? "EquipPrimary" : "EquipSecondary"
+            );
+        }
+    }
+}
 
 
 document.addEventListener('DOMContentLoaded', () => {
